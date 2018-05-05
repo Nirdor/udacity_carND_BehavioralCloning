c5997ba8b6e68f7fce196bcdd0765b04ea45f600
Martin Seidel <seidelmartin@gmx.de>
2018-05-05 20:32:45 +0200

    A

--- learn_nvd.py
+++ learn_nvd.py
@@ -28,6 +28,7 @@ with open('traindata/recover/driving_log.csv') as f:
   reader = csv.reader(f)
   for line in reader:
     lines.append((line, 0))
+    lines.append((line, 0))
     
 with open('traindata/additional/driving_log.csv') as f:
   reader = csv.reader(f)
@@ -71,7 +72,7 @@ y_train = np.array(measurements)
 
 print('Data ready. Create Model...')
 
-model_name = 'model_nvd07.h5'
+model_name = 'model_nvd09.h5'
 
 if os.path.exists(model_name):
   model = load_model(model_name, custom_objects = {'LRN':mod.LRN})
