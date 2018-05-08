20124f116f92908b7e8f0ee4c673425daca9857a
Martin Seidel <seidelmartin@gmx.de>
2018-05-08 11:17:23 +0200

    fix

--- drive.py
+++ drive.py
@@ -16,6 +16,7 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+import cv2
 from model import LRN
 
 sio = socketio.Server()
@@ -63,6 +64,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
